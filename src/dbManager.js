a832ec0ab8c9ea945270ad982bfce97320181254
idanGeula <idangeula7@gmail.coom>
2019-01-18 02:06:05 +0200

    shortcut for DbManager

--- src/dbManager.js
+++ src/dbManager.js
@@ -2,154 +2,44 @@ const dbOperations = require("./dbOperations");
 const studentCrudOperations = require("./studentCrudOperations");
 const courseCrudOperations = require("./courseCrudOperations");
 
-const dbInit = () => {
-    return new Promise((resolve, reject) => {
-        dbOperations.init().then(()=> {
-            resolve();
-        }).catch(() => {
-            reject();
-        });
-    });
-};
+const dbInit = () => (dbOperations.init());
 
 // Student CRUD operations
-const createStudent = (studentId, studentName) => {
-    return new Promise((resolve, reject) => {
-        studentCrudOperations.createStudent(studentId, studentName).then((data)=> {
-            resolve(data);
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const readStudent = (studentId) => {
-    return new Promise((resolve, reject) => {
-        studentCrudOperations.readStudent(studentId).then((data)=> {
-            resolve(data);
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const readAllStudents = () => {
-    return new Promise((resolve, reject) => {
-        studentCrudOperations.readAllStudents().then((data)=> {
-            resolve(data);
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const updateStudent = (studentId, newStudentName) => {
-    return new Promise((resolve, reject) => {
-        studentCrudOperations.updateStudent(studentId, newStudentName).then(()=> {
-            resolve();
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const deleteStudent = (studentId) => {
-    return new Promise((resolve, reject) => {
-        studentCrudOperations.deleteStudent(studentId).then(()=> {
-            resolve();
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
+const createStudent = (studentId, studentName) => (studentCrudOperations.createStudent(studentId, studentName));
+const readStudent = (studentId) => (studentCrudOperations.readStudent(studentId));
+const readAllStudents = () => (studentCrudOperations.readAllStudents());
+const updateStudent = (studentId, newStudentName) => (studentCrudOperations.updateStudent(studentId, newStudentName));
+const deleteStudent = (studentId) => (studentCrudOperations.deleteStudent(studentId));
 
 
 // Course CRUD operations
-const createCourse = (courseId, courseName) => {
-    return new Promise((resolve, reject) => {
-        courseCrudOperations.createCourse(courseId, courseName).then((data)=> {
-            resolve(data);
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const readCourse = (courseId) => {
-    return new Promise((resolve, reject) => {
-        courseCrudOperations.readCourse(courseId).then((data)=> {
-            resolve(data);
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const readAllCourses = () => {
-    return new Promise((resolve, reject) => {
-        courseCrudOperations.readAllCourses().then((data)=> {
-            resolve(data);
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const updateCourse = (courseId, newCourseName) => {
-    return new Promise((resolve, reject) => {
-        courseCrudOperations.updateCourse(courseId, newCourseName).then(()=> {
-            resolve();
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-const deleteCourse = (courseId) => {
-    return new Promise((resolve, reject) => {
-        courseCrudOperations.deleteCourse(courseId).then(()=> {
-            resolve();
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-};
-
-
+const createCourse = (courseId, courseName) => (courseCrudOperations.createCourse(courseId, courseName));
+const readCourse = (courseId) => (courseCrudOperations.readCourse(courseId));
+const readAllCourses = () => (courseCrudOperations.readAllCourses());
+const updateCourse = (courseId, newCourseName) => (courseCrudOperations.updateCourse(courseId, newCourseName));
+const deleteCourse = (courseId) => (courseCrudOperations.deleteCourse(courseId));
 
 // General methods
-const assignStudentToCourse = ((studentId, courseId) => {
-    return new Promise((resolve, reject) => {
-        dbOperations.assignStudentToCourse(studentId, courseId).then(()=> {
-            resolve();
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-});
+const assignStudentToCourse = (studentId, courseId) => (dbOperations.assignStudentToCourse(studentId, courseId));
+const setScore = (studentId, courseId, newGrade) => (dbOperations.setScore(studentId, courseId, newGrade));
+const getOutstandingStudents = () => (dbOperations.getOutstandingStudents());
 
-const setScore = (studentId, courseId, newGrade) => {
-    return new Promise((resolve, reject) => {
-        dbOperations.setScore(studentId, courseId, newGrade).then(()=> {
-            resolve();
-        }).catch((err) => {
-            reject(err);
-        });
-    });
+const closeConnection = () => (dbOperations.closeConnection());
+
+module.exports = {
+    dbInit,
+    createStudent,
+    readStudent,
+    readAllStudents,
+    updateStudent,
+    deleteStudent,
+    createCourse,
+    readCourse,
+    readAllCourses,
+    updateCourse,
+    deleteCourse,
+    assignStudentToCourse,
+    setScore,
+    getOutstandingStudents,
+    closeConnection
 };
-
-const getOutstandingStudents = () => {
-    return new Promise((resolve, reject) => {
-        dbOperations.getOutstandingStudents().then((result)=> {
-            resolve(result);
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-};
-
-const closeConnection = () => {
-    dbOperations.closeConnection();
-};
-
-module.exports = {dbInit, createStudent, readStudent, readAllStudents, updateStudent, deleteStudent, createCourse, readCourse, readAllCourses, updateCourse, deleteCourse, assignStudentToCourse, setScore, getOutstandingStudents, closeConnection};
